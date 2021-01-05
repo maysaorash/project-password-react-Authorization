dab248610dada835ebddb65d8968caefcbe73c21
maysaorash <72366798+maysaorash@users.noreply.github.com>
2021-01-05 17:45:36 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ const userInfo = {
   mail: "gmail@chucknorris.com",
   imgURL:
       "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-  phone: "+918 372 574"
+  phone: "+918 372 574",
+  password: "123"
 }
 
 class App extends React.Component {
